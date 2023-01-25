0d2ac00c4fb85eb7bb2a943b41bbc7e679e561d3
MerGr <fluffyspike3317@gmail.com>
2023-01-25 19:27:39 +0100

    Added UI JS code, need FIX

--- rps.js
+++ rps.js
@@ -1,46 +1,52 @@
-let Options = ['Rock', 'Paper', 'Scissors'];
+let Options = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice(){
     return Options[Math.floor(Math.random()*3)];
 }
 
-console.log(getComputerChoice());
+const rockbtn = document.querySelector(".rock"); rockbtn.textContent='rock';
+const paperbtn = document.querySelector(".paper"); paperbtn.textContent='paper';
+const scissorsbtn = document.querySelector(".scissors"); scissorsbtn.textContent='scissors';
+let PlayerChoice = [rockbtn, paperbtn, scissorsbtn];
+console.log(PlayerChoice);
 
-function OneRPSRound(PlayerSelection, CpuSelection){
-    let PlayerChoice=PlayerSelection.toLowerCase();
-    let CpuChoice=CpuSelection.toLowerCase();
-    if(PlayerChoice=='rock' && CpuChoice=='paper' 
-    || PlayerChoice=='paper' && CpuChoice=='scissors' 
-    || PlayerChoice=='scissors' && CpuChoice=='rock')
-        return 1;
-    if(PlayerChoice=='paper' && CpuChoice=='rock' 
-    || PlayerChoice=='scissors' && CpuChoice=='paper' 
-    || PlayerChoice=='rock' && CpuChoice=='scissors')
-        return 2;
+let CpuScore=0;
+let PlayerScore=0;
+let Count=0;
+
+document.querySelector('.cpuscore').textContent=CpuScore;
+document.querySelector('.plyrscore').textContent=PlayerScore;
+function OneRPSRound(PlayerChoice, CpuChoice){
+
+    PlayerChoice.forEach(option => {
+        option.addEventListener('click', function () {
+            console.log(this.textContent);
+            if (this.textContent.toLowerCase() == 'rock' && CpuChoice == 'paper'
+                || this.textContent.toLowerCase()== 'paper' && CpuChoice == 'scissors'
+                || this.textContent.toLowerCase() == 'scissors' && CpuChoice == 'rock')
+                ++CpuScore;
+            if (this.textContent.toLowerCase() == 'paper' && CpuChoice == 'rock'
+                || this.textContent.toLowerCase() == 'scissors' && CpuChoice == 'paper'
+                || this.textContent.toLowerCase() == 'rock' && CpuChoice == 'scissors')
+                ++PlayerScore;
+        });
+    });
+    ++Count;
 }
 
-console.log(OneRPSRound(Options[1], getComputerChoice()));
+
 
 function game(){
-    let CpuScore=0;
-    let PlayerScore=0;
-    for(let i = 0; i < 5; i++){
-        let getPlayerChoice=Options[prompt('1- Rock\n2- Paper\n3- Scissors\nChoose your weapon: ', Math.floor(Math.random()*4))-1];
-        let getCpuChoice=getComputerChoice();
-        switch (OneRPSRound(getPlayerChoice, getCpuChoice)) {
-            case 1:
-                ++CpuScore;
-                break;
-            case 2:
-                ++PlayerScore;
-                break;
-            default:
-                break;
-        }
-    }
-    if(CpuScore < PlayerScore) return 'YOU WIN!';
-    else if(CpuScore > PlayerScore) return 'YOU LOST';
-    else return 'DRAW!';
+
+    //for(let i = 0; i < 5; i++){
+
+        if(Count==0) OneRPSRound(PlayerChoice,getComputerChoice());
+        Count=0;
+    //}
+    if(CpuScore < PlayerScore) return  document.querySelector('.res_output').textContent = 'You WIN!';
+        else if(CpuScore > PlayerScore) return document.querySelector('.res_output').textContent = 'You LOSE!';
+        else return document.querySelector('.res_output').textContent = 'DRAW!'; 
+  
 }
 
-console.log(game());
+game();
