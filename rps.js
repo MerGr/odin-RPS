4c768373dfc3f82b3be6a26edf8429cd2a3a3173
MerGr <fluffyspike3317@gmail.com>
2023-01-27 20:45:11 +0100

    Added Color to UI

--- rps.js
+++ rps.js
@@ -4,9 +4,9 @@ function getComputerChoice(){
     return Options[Math.floor(Math.random()*3)];
 }
 
-const rockbtn = document.querySelector(".rock"); rockbtn.textContent='rock';
-const paperbtn = document.querySelector(".paper"); paperbtn.textContent='paper';
-const scissorsbtn = document.querySelector(".scissors"); scissorsbtn.textContent='scissors';
+const rockbtn = document.querySelector(".rock"); 
+const paperbtn = document.querySelector(".paper"); 
+const scissorsbtn = document.querySelector(".scissors"); 
 let PlayerChoice = [rockbtn, paperbtn, scissorsbtn];
 console.log(PlayerChoice);
 
@@ -18,15 +18,15 @@ let Count=0;
 
 function OneRPSRound(PlyrChoice, CpuChoice){
     console.log(PlyrChoice.textContent);
-    if (PlyrChoice.textContent == 'rock' && CpuChoice == 'paper'
-        || PlyrChoice.textContent== 'paper' && CpuChoice == 'scissors'
-        || PlyrChoice.textContent == 'scissors' && CpuChoice == 'rock'){
-        ++CpuScore; document.querySelector('.res_output').textContent = 'You LOSE!';}
-    else if (PlyrChoice.textContent == 'paper' && CpuChoice == 'rock'
-        || PlyrChoice.textContent == 'scissors' && CpuChoice == 'paper'
-        || PlyrChoice.textContent == 'rock' && CpuChoice == 'scissors') {
-        ++PlayerScore;document.querySelector('.res_output').textContent = 'You WIN!';}
-    else document.querySelector('.res_output').textContent = 'DRAW!';
+    if (PlyrChoice.textContent == 'Rock' && CpuChoice == 'paper'
+        || PlyrChoice.textContent== 'Paper' && CpuChoice == 'scissors'
+        || PlyrChoice.textContent == 'Scissors' && CpuChoice == 'rock'){
+        ++CpuScore; document.querySelector('#res_output').textContent = 'You LOSE!'; document.getElementById("res_output").style.color="red";}
+    else if (PlyrChoice.textContent == 'Paper' && CpuChoice == 'rock'
+        || PlyrChoice.textContent == 'Scissors' && CpuChoice == 'paper'
+        || PlyrChoice.textContent == 'Rock' && CpuChoice == 'scissors') {
+        ++PlayerScore;document.querySelector('#res_output').textContent = 'You WIN!'; document.getElementById("res_output").style.color="green";}
+    else { document.querySelector('#res_output').textContent = 'DRAW!';document.getElementById("res_output").style.color="orange";}
     removeEventListener('click', OneRPSRound);
 }
 
