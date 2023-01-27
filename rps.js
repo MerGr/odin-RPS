7a0ea3c16f16bfe1477166de9503687ed6d632de
MerGr <fluffyspike3317@gmail.com>
2023-01-27 12:31:36 +0100

    Fixed RPS, need FIX Multiple inputs BUG

--- rps.js
+++ rps.js
@@ -14,39 +14,38 @@ let CpuScore=0;
 let PlayerScore=0;
 let Count=0;
 
-document.querySelector('.cpuscore').textContent=CpuScore;
-document.querySelector('.plyrscore').textContent=PlayerScore;
-function OneRPSRound(PlayerChoice, CpuChoice){
 
+
+function OneRPSRound(PlayerChoice, CpuChoice){
     PlayerChoice.forEach(option => {
         option.addEventListener('click', function () {
             console.log(this.textContent);
-            if (this.textContent.toLowerCase() == 'rock' && CpuChoice == 'paper'
-                || this.textContent.toLowerCase()== 'paper' && CpuChoice == 'scissors'
-                || this.textContent.toLowerCase() == 'scissors' && CpuChoice == 'rock')
-                ++CpuScore;
-            if (this.textContent.toLowerCase() == 'paper' && CpuChoice == 'rock'
-                || this.textContent.toLowerCase() == 'scissors' && CpuChoice == 'paper'
-                || this.textContent.toLowerCase() == 'rock' && CpuChoice == 'scissors')
-                ++PlayerScore;
+            if (this.textContent == 'rock' && CpuChoice == 'paper'
+                || this.textContent== 'paper' && CpuChoice == 'scissors'
+                || this.textContent == 'scissors' && CpuChoice == 'rock'){
+                ++CpuScore; document.querySelector('.res_output').textContent = 'You LOSE!';}
+            if (this.textContent == 'paper' && CpuChoice == 'rock'
+                || this.textContent == 'scissors' && CpuChoice == 'paper'
+                || this.textContent == 'rock' && CpuChoice == 'scissors') {
+                ++PlayerScore;document.querySelector('.res_output').textContent = 'You WIN!';}
+            else document.querySelector('.res_output').textContent = 'DRAW!';
         });
     });
-    ++Count;
+    
 }
 
 
 
 function game(){
+        function disablebuttons(state){
+            for(i=0;i<3;i++)
+            PlayerChoice[i].disabled=!!state;
+        }
+        disablebuttons(true);
+        setTimeout(e=>{disablebuttons(false);},1000);
+        OneRPSRound(PlayerChoice,getComputerChoice());
+        document.querySelector('.cpuscore').textContent=CpuScore;
+        document.querySelector('.plyrscore').textContent=PlayerScore;
+        console.log("computer choice: "+getComputerChoice());;}
 
-    //for(let i = 0; i < 5; i++){
-
-        if(Count==0) OneRPSRound(PlayerChoice,getComputerChoice());
-        Count=0;
-    //}
-    if(CpuScore < PlayerScore) return  document.querySelector('.res_output').textContent = 'You WIN!';
-        else if(CpuScore > PlayerScore) return document.querySelector('.res_output').textContent = 'You LOSE!';
-        else return document.querySelector('.res_output').textContent = 'DRAW!'; 
-  
-}
-
-game();
+for(const button of PlayerChoice) {button.addEventListener("click",game); };
