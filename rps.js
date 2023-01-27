c312acc0089cbfb251fbe3f5dca53e49dfc84cbc
MerGr <fluffyspike3317@gmail.com>
2023-01-27 20:07:59 +0100

    Fixed RPS UI

--- rps.js
+++ rps.js
@@ -16,36 +16,36 @@ let Count=0;
 
 
 
-function OneRPSRound(PlayerChoice, CpuChoice){
-    PlayerChoice.forEach(option => {
-        option.addEventListener('click', function () {
-            console.log(this.textContent);
-            if (this.textContent == 'rock' && CpuChoice == 'paper'
-                || this.textContent== 'paper' && CpuChoice == 'scissors'
-                || this.textContent == 'scissors' && CpuChoice == 'rock'){
-                ++CpuScore; document.querySelector('.res_output').textContent = 'You LOSE!';}
-            if (this.textContent == 'paper' && CpuChoice == 'rock'
-                || this.textContent == 'scissors' && CpuChoice == 'paper'
-                || this.textContent == 'rock' && CpuChoice == 'scissors') {
-                ++PlayerScore;document.querySelector('.res_output').textContent = 'You WIN!';}
-            else document.querySelector('.res_output').textContent = 'DRAW!';
-        });
-    });
-    
+function OneRPSRound(PlyrChoice, CpuChoice){
+    console.log(PlyrChoice.textContent);
+    if (PlyrChoice.textContent == 'rock' && CpuChoice == 'paper'
+        || PlyrChoice.textContent== 'paper' && CpuChoice == 'scissors'
+        || PlyrChoice.textContent == 'scissors' && CpuChoice == 'rock'){
+        ++CpuScore; document.querySelector('.res_output').textContent = 'You LOSE!';}
+    else if (PlyrChoice.textContent == 'paper' && CpuChoice == 'rock'
+        || PlyrChoice.textContent == 'scissors' && CpuChoice == 'paper'
+        || PlyrChoice.textContent == 'rock' && CpuChoice == 'scissors') {
+        ++PlayerScore;document.querySelector('.res_output').textContent = 'You WIN!';}
+    else document.querySelector('.res_output').textContent = 'DRAW!';
+    removeEventListener('click', OneRPSRound);
+}
+
+function disablebuttons(state){
+    for(i=0;i<3;i++)
+    PlayerChoice[i].disabled=!!state;
 }
 
 
-
 function game(){
-        function disablebuttons(state){
-            for(i=0;i<3;i++)
-            PlayerChoice[i].disabled=!!state;
-        }
         disablebuttons(true);
         setTimeout(e=>{disablebuttons(false);},1000);
-        OneRPSRound(PlayerChoice,getComputerChoice());
+        let CpuChoice=getComputerChoice();
+        PlayerChoice[0].addEventListener('click', OneRPSRound(this,CpuChoice));
         document.querySelector('.cpuscore').textContent=CpuScore;
         document.querySelector('.plyrscore').textContent=PlayerScore;
-        console.log("computer choice: "+getComputerChoice());;}
+        console.log("computer choice: "+CpuChoice);
+        ++Count;
+    };
 
-for(const button of PlayerChoice) {button.addEventListener("click",game); };
+if(CpuScore<5 || PlayerScore<5) {for(const button of PlayerChoice) {button.addEventListener("click",game)};}
+else disablebuttons(true);
